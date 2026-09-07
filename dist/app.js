838f1802c48513b1ec1ed3aeb98d2eb19618d0cb
michkuc <michkuc@gmail.com>
2026-09-07 13:31:06 +0200

    Slow goalkeeper game for accessible play

--- dist/app.js
+++ dist/app.js
@@ -319,7 +319,7 @@ function startGoalGame() {
   $('#goalOverlay').classList.add('is-hidden');
   $('#goalBall').classList.add('is-visible');
   moveGoalBall();
-  goalMovement = window.setInterval(moveGoalBall, 950);
+  goalMovement = window.setInterval(moveGoalBall, 1300);
   goalTimer = window.setInterval(() => {
     remaining -= 1;
     $('#goalTime').textContent = String(remaining);

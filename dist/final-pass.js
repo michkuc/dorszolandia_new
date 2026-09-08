f7de973ee9624a5c5a40f32658e37f951ed30b0d
michkuc <michkuc@gmail.com>
2026-09-08 21:50:30 +0200

    Load complete canonical Dorszolandia data

--- dist/final-pass.js
+++ dist/final-pass.js
@@ -1,5 +1,7 @@
-// Finalny loader strony: istniejący pass, dopasowany kreator i kanoniczna biblioteka opowiadań.
+// Finalny loader strony: interaktywna kraina, kreator, kanoniczna mapa, pełny Atlas i 47 opowiadań.
 import './final-pass-core.js';
 import './creator-drive.js';
+import './atlas-upgrade.js';
+import './map-canon.js';
 import './library-upgrade.js';
 import './library-polish.js';

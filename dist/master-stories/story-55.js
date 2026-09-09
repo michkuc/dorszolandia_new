1fce960ddd2b9efb6b02dd006b824822718ca955
michkuc <michkuc@gmail.com>
2026-09-09 18:23:07 +0200

    Load MASTER FINAL story 55

---
+++ dist/master-stories/story-55.js
@@ -0,0 +1,5 @@
+import encoded from './story-55-data.js';
+const binary = atob(encoded);
+const bytes = Uint8Array.from(binary, character => character.charCodeAt(0));
+const stream = new Blob([bytes]).stream().pipeThrough(new DecompressionStream('gzip'));
+export default JSON.parse(await new Response(stream).text());

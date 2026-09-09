fd83c798d45cddd5ee4dce8f8d6c4797c626ff12
michkuc <michkuc@gmail.com>
2026-09-09 10:03:04 +0200

    Remove redundant observer layer from production loader

--- dist/final-pass.js
+++ dist/final-pass.js
@@ -1,7 +1,7 @@
 // Finalny loader strony: interaktywna kraina, kreator, kanoniczna mapa, pełny Atlas i 47 opowiadań.
+// 2026-09-09: usunięto redundantny library-polish.js, który mógł blokować główny wątek przez obserwatory DOM.
 import './final-pass-core.js';
 import './creator-drive.js';
 import './atlas-upgrade.js';
 import './map-canon.js';
 import './library-upgrade.js';
-import './library-polish.js';
